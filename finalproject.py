fe2475258c935715f3d08c1733addd5439fd1d50
Avery Chezem <chezema21@hanover.edu>
2018-12-14 16:44:04 -0500

    final version

--- finalproject.py
+++ finalproject.py
@@ -49,7 +49,7 @@ def getTeamId(teams, name):
 #
 def makeObjectfromitem(item):
    tds = item.find_all('td')
-   return {'Position': tds[0].get_text(), 'Number' : tds[1].get_text(), 'Name':tds[2].get_text()}
+   return {'Position': tds[0].get_text()[:-1], 'Name':tds[2].get_text()[:-1], 'Number':tds[1].get_text()[20:-1], 'From':tds[6].get_text()[:-1]}
 
 
 # players (
@@ -61,112 +61,115 @@ def getPlayerFromTeam(team):
    ]
 
 anyTeam = getPlayerFromTeam(teams[getTeamId(teams, inputname)].next_sibling.next_sibling)
-print(anyTeam)
+# print(anyTeam)
 
-#____________________________________________________________________________
-# #TWITTER
-# #!/usr/bin/python3
-# # Accessing the Twitter API
-# # This script describes the basic methodology for accessing a Twitter feed
-# # or something similar.
+with open('NBA_info_Wikipedia.json', 'w') as file:
+  json.dump(anyTeam, file)
 
-# # Loading libraries needed for authentication and requests
-# from requests_oauthlib import OAuth2Session
-# from oauthlib.oauth2 import BackendApplicationClient
-# import json
+# ____________________________________________________________________________
+#TWITTER
+#!/usr/bin/python3
+# Accessing the Twitter API
+# This script describes the basic methodology for accessing a Twitter feed
+# or something similar.
 
-# # In order to use this script, you must:
-# # - Have a Twitter account and create an app
-# # - Store in keys.json a property called "twitter" whose value is an
-# #     object with two keys, "key" and "secret"
-# with open('keys.json', 'r') as f:
-#    keys = json.loads(f.read())['twitter']
+# Loading libraries needed for authentication and requests
+from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2 import BackendApplicationClient
+import json
 
-# twitter_key = keys['key']
-# twitter_secret = keys['secret']
+# In order to use this script, you must:
+# - Have a Twitter account and create an app
+# - Store in keys.json a property called "twitter" whose value is an
+#     object with two keys, "key" and "secret"
+with open('keys.json', 'r') as f:
+   keys = json.loads(f.read())['twitter']
 
-# # We authenticate ourselves with the above credentials
-# # We will demystify this process later
-# #
-# # For documentation, see http://requests-oauthlib.readthedocs.io/en/latest/api.html
-# # and http://docs.python-requests.org/en/master/
-# client = BackendApplicationClient(client_id=twitter_key)
-# oauth = OAuth2Session(client=client)
-# token = oauth.fetch_token(token_url='https://api.twitter.com/oauth2/token',
-#                           client_id=twitter_key,
-#                           client_secret=twitter_secret)
+twitter_key = keys['key']
+twitter_secret = keys['secret']
 
-# # Base url needed for all subsequent queries
-# base_url = 'https://api.twitter.com/1.1/'
+# We authenticate ourselves with the above credentials
+# We will demystify this process later
+#
+# For documentation, see http://requests-oauthlib.readthedocs.io/en/latest/api.html
+# and http://docs.python-requests.org/en/master/
+client = BackendApplicationClient(client_id=twitter_key)
+oauth = OAuth2Session(client=client)
+token = oauth.fetch_token(token_url='https://api.twitter.com/oauth2/token',
+                          client_id=twitter_key,
+                          client_secret=twitter_secret)
 
-# # Particular page requested. The specific query string will be
-# # appended to that.
-# page = 'search/tweets.json'
+# Base url needed for all subsequent queries
+base_url = 'https://api.twitter.com/1.1/'
 
-# #Makes query with input from user
-# query = '?q=%s&tweet_mode=extended&count=100'%inputname
+# Particular page requested. The specific query string will be
+# appended to that.
+page = 'search/tweets.json'
 
-# # Depending on the query we are interested in, we append the necessary string
-# # As you read through the twitter API, you'll find more possibilities
-# req_url = base_url + page + query
+#Makes query with input from user
+query = '?q=%s&tweet_mode=extended&count=100'%inputname
 
-# # We perform a request. Contains standard HTTP information
-# response = oauth.get(req_url)
+# Depending on the query we are interested in, we append the necessary string
+# As you read through the twitter API, you'll find more possibilities
+req_url = base_url + page + query
 
-# # Read the query results
-# results = json.loads(response.content.decode('utf-8'))
+# We perform a request. Contains standard HTTP information
+response = oauth.get(req_url)
 
-# ## Process the results
-# ## CAUTION: The following code will attempt to read up to 10000 tweets that
-# ## Mention Hanover College. You should NOT change this code.
-# tweets = results['statuses']
-# while True:
-#    if not ('next_results' in results['search_metadata']):
-#       break
-#    if len(tweets) > 10000:
-#       break
-#    next_search = base_url + page + results['search_metadata']['next_results'] + '&tweet_mode=extended'
-#    print(results['search_metadata']['next_results'])
-#    response = oauth.get(next_search)
-#    results = json.loads(response.content.decode('utf-8'))
-#    tweets.extend(results['statuses'])
+# Read the query results
+results = json.loads(response.content.decode('utf-8'))
 
-# ## CAUTION: For the rest of this assignment, the list "tweets" contains all the
-# ## tweets you would want to work with. Do NOT change the list or the value of "tweets".
+## Process the results
+## CAUTION: The following code will attempt to read up to 10000 tweets that
+## Mention Hanover College. You should NOT change this code.
+tweets = results['statuses']
+while True:
+   if not ('next_results' in results['search_metadata']):
+      break
+   if len(tweets) > 10000:
+      break
+   next_search = base_url + page + results['search_metadata']['next_results'] + '&tweet_mode=extended'
+   print(results['search_metadata']['next_results'])
+   response = oauth.get(next_search)
+   results = json.loads(response.content.decode('utf-8'))
+   tweets.extend(results['statuses'])
 
-# ## list comprehension that will produce a list containing all the texts from the tweets
-# texts = [tweet['full_text'] for tweet in tweets]
+## CAUTION: For the rest of this assignment, the list "tweets" contains all the
+## tweets you would want to work with. Do NOT change the list or the value of "tweets".
 
-# ## returns the full texts of the tweets
-# def get_full_text(tweet):
-#   if 'retweeted_status' in tweet.keys():
-#     full_text = tweet['retweeted_status']['full_text']
-#     return(full_text)
-#   else:
-#     full_text = tweet['full_text']
-#     return(full_text)
+## list comprehension that will produce a list containing all the texts from the tweets
+texts = [tweet['full_text'] for tweet in tweets]
 
-# full_texts_list = [get_full_text(tweet) for tweet in tweets]
+## returns the full texts of the tweets
+def get_full_text(tweet):
+  if 'retweeted_status' in tweet.keys():
+    full_text = tweet['retweeted_status']['full_text']
+    return(full_text)
+  else:
+    full_text = tweet['full_text']
+    return(full_text)
 
-# def tags_per_tweet_function(tweet):
-#   hashtag_list = []
-#   hashtags = tweet['entities']['hashtags']
-#   for i in range(len(hashtags)):
-#       hashtag_list.append(tweet['entities']['hashtags'][i]['text'])
-#   return hashtag_list
+full_texts_list = [get_full_text(tweet) for tweet in tweets]
 
-# tags_per_tweet = [tags_per_tweet_function(tweet) for tweet in tweets]
+def tags_per_tweet_function(tweet):
+  hashtag_list = []
+  hashtags = tweet['entities']['hashtags']
+  for i in range(len(hashtags)):
+      hashtag_list.append(tweet['entities']['hashtags'][i]['text'])
+  return hashtag_list
 
-# def tweet_information_function(tweet):
-#   tweet_dict = {}
-#   tweet_dict['text'] = get_full_text(tweet)
-#   tweet_dict['author'] = tweet['user']['screen_name']
-#   tweet_dict['date'] = tweet['created_at']
-#   tweet_dict['hashtags'] = tags_per_tweet_function(tweet)
-#   tweet_dict['mentions'] = [user['screen_name'] for user in tweet['entities']['user_mentions']]
-#   return tweet_dict
+tags_per_tweet = [tags_per_tweet_function(tweet) for tweet in tweets]
 
-# tweet_information = [tweet_information_function(tweet) for tweet in tweets]
+def tweet_information_function(tweet):
+  tweet_dict = {}
+  tweet_dict['text'] = get_full_text(tweet)
+  tweet_dict['author'] = tweet['user']['screen_name']
+  tweet_dict['date'] = tweet['created_at']
+  tweet_dict['hashtags'] = tags_per_tweet_function(tweet)
+  tweet_dict['mentions'] = [user['screen_name'] for user in tweet['entities']['user_mentions']]
+  return tweet_dict
 
-# with open('NBA_Team_Tweets.json', 'w') as file:
-#   json.dump(tweet_information, file)
+tweet_information = [tweet_information_function(tweet) for tweet in tweets]
+
+with open('NBA_Info_Twitter.json', 'w') as file:
+  json.dump(tweet_information, file)
